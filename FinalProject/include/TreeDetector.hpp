5aeb62da2edef68ecf4ded4c07c9d96acfe27a59
Davide Pistilli <davidepistilli173@gmail.com>
2020-06-04 12:07:48 +0200

    Add initial preprocessing

--- FinalProject/include/TreeDetector.hpp
+++ FinalProject/include/TreeDetector.hpp
@@ -1,26 +1,55 @@
 #ifndef TREEDETECTOR_HPP
 #define TREEDETECTOR_HPP
 
-#include <opencv2/core.hpp>
+#include "utility.hpp"
 
-namespace prj {
+#include <array>
+#include <opencv2/core.hpp>
+#include <variant>
+
+namespace prj
+{
    // Detect and locate trees in an image.
-   class TreeDetector {
+   class TreeDetector
+   {
    public:
+      /********** ENUMS **********/
+      // Preprocessing parameters.
+      enum class PParam
+      {
+         bi_size,     // Size of the bilateral filter.
+         bi_colour_s, // Colour sigma for the bilateral filter.
+         bi_space_s,  // Space sigma for the bilateral filter.
+         tot          // Total number of parameters.
+      };
+      // Analysis parameters.
+      enum class AParam
+      {
+         tot
+      };
+
+      /********** CONSTANTS **********/
+      static const cv::Size analysis_res; // Resolution used to analyse the image.
+
+      /********** CONSTRUCTOR **********/
+      TreeDetector() = default;
+
+      /********** METHODS **********/
       // Detect trees and output the result.
       cv::Mat detect(cv::Mat input);
 
    private:
       /********** METHODS **********/
       // Analyse the preprocessed data to find trees.
-      bool analyse_();
+      bool analyse_(std::array<param, static_cast<int>(AParam::tot)>& params);
       // Draw the final result.
       bool drawResult_();
       // Preprocess the input image.
-      bool preProcess_();
+      bool preProcess_(std::array<param, static_cast<int>(PParam::tot)>& params);
 
       /********** VARIABLES **********/
-      cv::Mat result_;
+      cv::Mat resizedInput_; // Resized input image.
+      cv::Mat result_;       // Final result.
    };
 } // namespace prj
 
--- FinalProject/src/TreeDetector.cpp
+++ FinalProject/src/TreeDetector.cpp
@@ -1,24 +1,44 @@
 #include "TreeDetector.hpp"
 
-#include "utility.hpp"
+#include <opencv2/imgproc.hpp>
 
 using namespace prj;
 
-cv::Mat TreeDetector::detect(cv::Mat input) {
+const cv::Size TreeDetector::analysis_res{ 500, 500 };
+
+cv::Mat TreeDetector::detect(cv::Mat input)
+{
+   Log::info("Resizing image.");
+   // Used to convert tree coordinates back to the input's coordinate frame.
+   std::pair scalingFactor{
+      static_cast<float>(input.cols) / analysis_res.width,
+      static_cast<float>(input.rows) / analysis_res.height
+   };
+   cv::resize(input, resizedInput_, analysis_res);
+
    Log::info("Preprocessing.");
-   if (!preProcess_()) {
+   std::array<param, static_cast<int>(PParam::tot)> pParams{
+      29,
+      150.0,
+      100.0
+   };
+   if (!preProcess_(pParams))
+   {
       Log::error("Preprocessing failed.");
       return cv::Mat{};
    }
 
    Log::info("Analysing data.");
-   if (!analyse_()) {
+   std::array<param, static_cast<int>(AParam::tot)> aParams;
+   if (!analyse_(aParams))
+   {
       Log::error("Failed to analyse data.");
       return cv::Mat{};
    }
 
    Log::info("Drawing result.");
-   if (!drawResult_()) {
+   if (!drawResult_())
+   {
       Log::error("Failed to draw the final result.");
       return cv::Mat{};
    }
@@ -26,14 +46,40 @@ cv::Mat TreeDetector::detect(cv::Mat input) {
    return result_;
 }
 
-bool TreeDetector::analyse_() {
+bool TreeDetector::analyse_(std::array<param, static_cast<int>(AParam::tot)>& params)
+{
    return true;
 }
 
-bool TreeDetector::drawResult_() {
+bool TreeDetector::drawResult_()
+{
    return true;
 }
 
-bool TreeDetector::preProcess_() {
+bool TreeDetector::preProcess_(std::array<param, static_cast<int>(PParam::tot)>& params)
+{
+   cv::Mat filteredImg;
+   cv::Mat hsvInputImg;
+   cv::cvtColor(resizedInput_, hsvInputImg, cv::COLOR_BGR2HSV);
+
+   switch (resizedInput_.type)
+   {
+   case CV_8UC1:
+      cv::equalizeHist(resizedInput_, filteredImg);
+      break;
+   default:
+      equaliseImg(hsvInputImg);
+      cv::cvtColor(hsvInputImg, filteredImg, cv::COLOR_HSV2BGR);
+   }
+
+   cv::bilateralFilter(
+      filteredImg,
+      filteredImg,
+      std::get<int>(params[static_cast<int>(PParam::bi_size)]),
+      std::get<double>(params[static_cast<int>(PParam::bi_colour_s)]),
+      std::get<double>(params[static_cast<int>(PParam::bi_space_s)]));
+
+   displayImage(resizedInput_);
+   displayImage(filteredImg);
    return true;
 }
